cabf6e68bdae3e1af27d342a3f5a94e235ce7141
zhaoxuan <xuanzhao@88.com>
2021-11-04 00:25:35 +0800

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, ConfigEnv } from 'vite'
+import type { UserConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
+function pathResolve(dir: string) {
+  // 需要安装@types/node解决process类型报错
+  return resolve(process.cwd(), '.', dir);
+}
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+// export default defineConfig({
+//   plugins: [vue()]
+// })
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  return {
+    root,
+    resolve: {
+      alias: [
+        // /@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        // /#/xxxx => types/xxxx
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    }
+    // plugins: [vue()]
+  }
 })
+
