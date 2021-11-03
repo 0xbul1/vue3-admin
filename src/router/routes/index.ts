0f63cd59904a34620d1c1a180234ca9a281eea7a
赵旋 <>
2021-11-03 17:55:24 +0800

    add: add some config

---
+++ src/router/routes/index.ts
@@ -0,0 +1,10 @@
+import type { AppRouteRecordRaw } from '/@/router/types';
+
+export const RootRoute: AppRouteRecordRaw = {
+  path: '/',
+  name: 'Root',
+  redirect: PageEnum.BASE_HOME,
+  meta: {
+    title: 'Root',
+  },
+};
